eca37434bd755db7baec48f566394028f91246c2
Jason Coleman <jason@strangerstudios.com>
2015-03-20 14:29:04 -0400

    Fix to cap names.

--- pmpro-membership-manager-role.php
+++ pmpro-membership-manager-role.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: PMPro Membership Manager Role
+Plugin Name: Paid Memberships Pro - Membership Manager Role Add On
 Plugin URI: http://www.paidmembershipspro.com/pmpro-membership-manager-role/
 Description: Adds a Membership Manager role to WordPress with access to PMPro settings and reports.
-Version: .1
+Version: .1.1
 Author: Stranger Studios
 Author URI: http://www.strangerstudios.com
 */
@@ -23,7 +23,7 @@ function pmprommr_activation()
         'pmpro_advancedsettings' => true, 
         'pmpro_addons' => true,
 		'pmpro_memberslist' => true,
-        'pmpro_membersliscsv' => true,        
+        'pmpro_memberslistcsv' => true,        
         'pmpro_reports' => true,
 		'pmpro_orders' => true,
         'pmpro_orderscsv' => true,
@@ -38,3 +38,19 @@ function pmprommr_deactivation()
 }
 register_activation_hook(__FILE__, 'pmprommr_activation');
 register_deactivation_hook(__FILE__, 'pmprommr_deactivation');
+
+/*
+Function to add links to the plugin row meta
+*/
+function pmprommr_plugin_row_meta($links, $file) {
+	if(strpos($file, 'pmpro-membership-manager-role.php') !== false)
+	{
+		$new_links = array(
+			'<a href="' . esc_url('http://www.paidmembershipspro.com/add-ons/plugins-on-github/pmpro-membership-manager-role/')  . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro' ) . '</a>',
+			'<a href="' . esc_url('http://paidmembershipspro.com/support/') . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro' ) . '</a>',
+		);
+		$links = array_merge($links, $new_links);
+	}
+	return $links;
+}
+add_filter('plugin_row_meta', 'pmprommr_plugin_row_meta', 10, 2);
